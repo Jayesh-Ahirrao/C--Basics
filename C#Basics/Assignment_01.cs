a1d0bbc565874d99a528cf259e70cdc455b07817
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2024-12-14 16:45:24 +0530

    C# Basics : Assignment 01 - helloworld

---
+++ C#Basics/Assignment_01.cs
@@ -0,0 +1,19 @@
+﻿namespace C_Basics
+{
+    internal class Assignment_01
+    {
+        static void Main(string[] args) { 
+            System.Console.WriteLine("Enter your name : ");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Enter first num: ");
+            int num1 = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Enter Second num: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your name is:  " + name);
+
+            Console.WriteLine("Addition is:  " +( num1 + num2));
+
+        }
+    }
+}
