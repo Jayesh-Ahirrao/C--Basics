63a2c15798272680935d96eb712253d59ffc559a
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2024-12-17 16:55:12 +0530

    C# Basics : Assignment 10

---
+++ Assignment 10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] strs = { "one", "two", "three", "one", "four", "one" };
+
+            for(int i = 0; i < strs.Length -1; i++)
+            {
+                bool isDusplicate = false;
+
+                for(int j = 1; j < strs.Length; j++)
+                {
+                    if(i != j &&  strs[j] == strs[i])
+                    {
+                        isDusplicate = true; 
+                        break;
+                    }
+                }
+
+                if (!isDusplicate)
+                {
+                    Console.WriteLine(strs[i]);
+                }
+            }
+        }
+    }
+}
