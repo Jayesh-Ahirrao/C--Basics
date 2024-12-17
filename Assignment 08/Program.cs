b37c14c08ef3a73edc585e2aefeb3460e9c463a7
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2024-12-17 16:41:26 +0530

    C# Basics : Assignment 08

---
+++ Assignment 08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace Assignment_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int colWidth = 4;
+            for(int i = 1; i <= 12; i++)
+            {
+
+                for(int j = 1; j <= 10; j++)
+                {
+                    Console.Write($"{(j*i).ToString().PadLeft(colWidth)}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
