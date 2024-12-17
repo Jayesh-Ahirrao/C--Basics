4838ae588ce0fbb6ff0880a005c74e62bb08ce8a
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2024-12-17 16:47:25 +0530

    C# Basics : Assignment 09

---
+++ Assignment 09/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_09
+{
+    internal class Program
+    {
+        public static bool checkPalindrome(int num)
+        {
+            int numCopy = num;
+            int rev = 0;
+
+            while(numCopy > 0)
+            {
+                int rem = num % 10;
+                rev = rem * 10 + rem;
+                numCopy /= 10;
+            }
+
+            return rev == num;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(checkPalindrome(num));
+        }
+    }
+}
