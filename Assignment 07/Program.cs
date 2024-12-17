0e0fdd58086a27eb6b87065d0be6887e84d80aa4
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2024-12-17 16:39:15 +0530

    C# Basics : Assignment 07

---
+++ Assignment 07/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int columnWidth = 4;
+            for (int i = 1; i <= 10; i++)
+            {
+                for(int j = 1; j <= 12; j++)
+                {
+                    Console.Write($"{(i*j).ToString().PadLeft(columnWidth)}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
